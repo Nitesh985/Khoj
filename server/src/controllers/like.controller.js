e7f85e0eb345fe0243e6f2e9fe94752d89be95f3
Nitesh Acharya <Niteshacharya796@gmail.com>
2024-06-15 21:21:35 +0545

    made some changes in the like controller

--- server/src/controllers/like.controller.js
+++ server/src/controllers/like.controller.js
@@ -5,31 +5,77 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 const toggleLike = asyncHandler(async (req, res)=>{
     const userId = req.user?._id
-    const {destinationId, commentId} = req.params
+    const {destinationId} = req.params
 
     if (!userId){
         throw new ApiError(401, "The user is not authorised!")
     }
 
-    if (!destinationId && !commentId){
-        throw new ApiError(404, "The destination id or comment id was not found!")
+    if (!destinationId){
+        throw new ApiError(404, "The destination was not found!")
     }
 
-    const findLikedDestination = await Like.findOneAndUpdate({
+    const findLikedDestination = await Like.findOneAndDelete({
         $and:[{userId}, {destinationId}]
     })
 
-    if (!findLikedDestination ){
-        await Like.create({})
+
+    if (!findLikedDestination){
+        await Like.create({
+            userId,
+            destinationId
+        })
     }
 
-    const findLikedComment = await Like.findOne({
+    return res.status(200)
+    .json(
+        new ApiResponse(200, "The like was toggled")
+    )
+})
+
+
+const toggleCommentLike = asyncHandler(async(req, res)=>{
+    const userId = req.user?._id
+
+    const { commentId } = req.params
+
+    if (!commentId){
+        throw new ApiError(404, "The given comment was not found!")
+    }
+
+    const findLikedComments = await Like.findOneAndDelete({
         $and:[{userId}, {commentId}]
-    }) 
+    })
 
-    
+    if (!findLikedComments){
+        await Like.create({
+            userId,
+            commentId
+        })
+    }
 
+    return res.status(200)
+    .json(
+        new ApiResponse(200, "The comment like was successfully toggled")
+    )
 
+})
 
+const getLikedDestinations = asyncHandler(async (req, res)=>{
+    const {userId} = req.user?._id
 
+    if (!userId){
+        throw new ApiError(401, "The user is not authenticated to do such action")
+    }
+
+    await Like.aggregate([
+        {
+            $lookup:{
+                from:"destinations",
+                localField:"destinationId",
+                foreignField:"_id",
+                as:"destinations"
+            }
+        }
+    ])
 })
