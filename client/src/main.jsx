95f88ed5f3b76f5d815c49c229c3b5eb925ece14
Nitesh Acharya <Niteshacharya796@gmail.com>
2024-06-15 15:54:57 +0545

    done some routing setups

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Explore from './pages/Explore.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="" element={<App />} />
+      <Route path="explore" element={<Explore />} />
+    </Route>
+  )
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
