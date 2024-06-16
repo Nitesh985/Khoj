7765b751259badcc7dd07dd19b76848c0cd1985b
Nitesh Acharya <Niteshacharya796@gmail.com>
2024-06-16 09:24:11 +0545

    solved some import errors

--- server/src/routes/like.routes.js
+++ server/src/routes/like.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getLikedDestinations, toggleCommentLike, toggleLike } from "../controllers/like.controller";
+import { getLikedDestinations, toggleCommentLike, toggleLike } from "../controllers/like.controller.js";
 
 const router = Router()
 
